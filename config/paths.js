f611c1e8dec7d6856630d9e2d54319a7c58ac914
Andrew Gurylev <andrew@gurylev.com>
2015-11-04 08:04:08 +0300

    add paths config;

---
+++ config/paths.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+    dev: {
+        styles: [
+            './blocks/layout/layout.pcss',
+            './blocks/header/header.pcss',
+        ]
+    },
+    dist: {
+        styles: 'public/css'
+    }
+};
