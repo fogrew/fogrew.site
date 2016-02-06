67c10c18a298a09466b173a3b1138df7c58e9468
Andrew Gurylev <andrew@gurylev.com>
2016-02-06 16:55:45 +0300

    replace flatten plugin to rename path;

--- tasks/images.js
+++ tasks/images.js
@@ -6,7 +6,7 @@ module.exports = function(gulp, bs) {
   return gulp.task('images', function() {
     return gulp.src(paths.dev.images)
         .pipe(require('gulp-changed')(paths.dist.images))
-        .pipe(require('gulp-flatten')())
+        .pipe(require('gulp-rename')({dirname: ''}))
         .pipe(gulp.dest(paths.dist.images))
         .pipe(bs.stream());
   });
