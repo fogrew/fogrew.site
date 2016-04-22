2f8560857ff5c34d92a83537109a70e1f1d401c9
Andrew Gurylev <andrew@gurylev.com>
2016-04-22 08:05:43 +0300

    🚦 setup automatic router;

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 var express = require('express');
-var engine = require('ejs-locals');
-
 var app = express();
-
-app.engine('ejs', engine);
 
 app.set('root', __dirname);
 
-app.set('views', 'views');
 app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 6969);
+
 
 app.use(express.static('./public'));
 
-app.get('/', function(req, res, next) {
-    var locals = {};
-    locals.title = 'Andrew Gurylev';
-    res.render('layout/layout', locals);
+const pages = require('./config/routes.js');
+
+Object.keys(pages).forEach(page => {
+  app.get(page, function(req, res, next) {
+    res.render(pages[page]['layout'], pages[page]);
+  });
 });
 
-app.listen(6969);
+app.listen(app.get('port'), () => {
+  console.log('Site https://fogrew.site/ launch on port ' + app.get('port'));
+});
---
+++ config/routes.js
@@ -0,0 +1,19 @@
+'use strict';
+const works = require('./works');
+const home = require('./home');
+
+module.exports = {
+  '/': {
+    title: 'Fogrew @ Andrew Gurylev',
+    description: 'thoughtful, attentive, neat, careful, heedful, mindful, solicitous, regardful frontend developer',
+    layout: 'layout/layout',
+    content: '../about/about'
+  },
+  '/home': {
+    title: 'Homepage',
+    description: 'homepage for my browser',
+    layout: 'homepage/homepage',
+    content: '../grid/grid',
+    list: home
+  }
+};
