28c9603886946be0ad6c13331ca30aacef9e3761
Andrew Gurylev <andrew@gurylev.com>
2016-04-22 08:09:27 +0300

    🚀 add dashboard;

---
+++ config/home.js
@@ -0,0 +1,214 @@
+'use strict';
+
+module.exports = [
+  {
+    name: 'yandex',
+    title: 'Yandex',
+    type: 'svg',
+    url: 'https://yandex.ru/',
+    tags: ''
+  },
+  {
+    name: 'ymail',
+    title: 'Yandex Mail',
+    type: 'svg',
+    url: 'https://mail.yandex.ru/',
+    tags: ''
+  },
+  {
+    name: 'ymusic',
+    title: 'Yandex Music',
+    type: 'svg',
+    url: 'https://music.yandex.ru/',
+    tags: ''
+  },
+  {
+    name: 'ymoney',
+    title: 'Yandex Money',
+    type: 'svg',
+    url: 'https://money.yandex.ru/',
+    tags: ''
+  },
+  {
+    name: 'ymaps',
+    title: 'Yandex Maps',
+    type: 'svg',
+    url: 'https://maps.yandex.ru/',
+    tags: ''
+  },
+  {
+    name: 'ytranslate',
+    title: 'Yandex Translate',
+    type: 'svg',
+    url: 'https://translate.yandex.ru/',
+    tags: ''
+  },
+  {
+    name: 'youtube',
+    title: 'Youtube',
+    type: 'svg',
+    url: 'https://youtube.com/',
+    tags: ''
+  },
+  {
+    name: 'gmail',
+    title: 'Gmail',
+    type: 'svg',
+    url: 'https://gmail.com/',
+    tags: ''
+  },
+  {
+    name: 'gdrive',
+    title: 'Google Drive',
+    type: 'svg',
+    url: 'https://drive.google.com/',
+    tags: ''
+  },
+  {
+    name: 'gphotos',
+    title: 'Google Photos',
+    type: 'svg',
+    url: 'https://photos.google.com/',
+    tags: ''
+  },
+  {
+    name: 'gtranslate',
+    title: 'Google Translate',
+    type: 'svg',
+    url: 'https://translate.google.com/',
+    tags: ''
+  },
+  {
+    name: 'vk',
+    title: 'vk',
+    type: 'svg',
+    url: 'https://new.vk.com/',
+    tags: ''
+  },
+  {
+    name: 'twitter',
+    title: 'Twitter',
+    type: 'svg',
+    url: 'https://twitter.com',
+    tags: ''
+  },
+  {
+    name: 'lingualeo',
+    title: 'LinguaLeo',
+    type: 'png',
+    url: 'https://lingualeo.com/',
+    tags: ''
+  },
+  {
+    name: 'duolingo',
+    title: 'DuoLingo',
+    type: 'svg',
+    url: 'https://www.duolingo.com/',
+    tags: ''
+  },
+  {
+    name: 'facebook',
+    title: 'Facebook',
+    type: 'svg',
+    url: 'https://www.facebook.com/',
+    tags: ''
+  },
+  {
+    name: 'skype',
+    title: 'Skype',
+    type: 'svg',
+    url: 'https://web.skype.com/',
+    tags: ''
+  },
+  {
+    name: 'telegram',
+    title: 'Telegram',
+    type: 'svg',
+    url: 'https://web.telegram.org/',
+    tags: ''
+  },
+  {
+    name: 'calendar',
+    title: 'Calendar Mail.ru',
+    type: 'svg',
+    url: 'https://calendar.mail.ru/',
+    tags: ''
+  },
+  {
+    name: 'lastfm',
+    title: 'Last.fm',
+    type: 'svg',
+    url: 'http://www.last.fm/ru/user/Kasumi_Candor',
+    tags: ''
+  },
+  {
+    name: 'bitbucket',
+    title: 'Bitbucket',
+    type: 'svg',
+    url: 'https://bitbucket.org/',
+    tags: ''
+  },
+  {
+    name: 'codepen',
+    title: 'Codepen',
+    type: 'svg',
+    url: 'https://codepen.io/',
+    tags: ''
+  },
+  {
+    name: 'github',
+    title: 'Github',
+    type: 'svg',
+    url: 'https://github.com/',
+    tags: ''
+  },
+  {
+    name: 'iconfinder',
+    title: 'Iconfinder',
+    type: 'svg',
+    url: 'https://www.iconfinder.com/',
+    tags: ''
+  },
+  {
+    name: 'unicredit',
+    title: 'UniCredit',
+    type: 'svg',
+    url: 'https://enter.unicredit.ru/',
+    tags: ''
+  },
+  {
+    name: 'tinkoff',
+    title: 'Tinkoff Bank',
+    type: 'svg',
+    url: 'https://tinkoff.ru/mybank/',
+    tags: ''
+  },
+  {
+    name: 'rutracker',
+    title: 'RuTracker',
+    type: 'png',
+    url: 'https://rutracker.net/',
+    tags: ''
+  },
+  {
+    name: 'mactorrent',
+    title: 'Mac torrent',
+    type: 'svg',
+    url: 'http://www.mac-torrent-download.net/',
+    tags: ''
+  },
+  {
+    name: 'noname',
+    title: 'NoName Club',
+    type: 'png',
+    url: 'https://nnmclub.to/',
+    tags: ''
+  },
+  {
+    name: 'rutor',
+    title: 'RUTOR',
+    type: 'png',
+    url: 'http://new-rutor.org/',
+    tags: ''
+  },
+];
--- config/paths.js
+++ config/paths.js
@@ -8,14 +8,17 @@ module.exports = {
   distDir: dist,
   dev: {
     css:  views+'/**/*.pcss',
-    styles: views+'layout/style.pcss',
+    styles: [
+      views+'layout/style.pcss',
+      views+'homepage/homepage.pcss',
+    ],
     scripts: [
-      views+'layout/header.js',
       views+'layout/layout.js'
     ],
     images: views+'**/*.{jpg,png,svg}'
   },
   dist: {
+      pages: dist,
       styles: dist+'css',
       scripts: dist+'js',
       images: dist+'i'
