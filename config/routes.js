239116e5f41877a0128c9b3d6017e54587c8b746
Andrew Gurylev <andrey.gurylev@team.wrike.com>
2016-07-25 11:02:09 +0300

    👓 fix meta-information;

--- config/routes.js
+++ config/routes.js
@@ -4,12 +4,14 @@ const home = require('./home');
 module.exports = {
   '/': {
     title: 'Fogrew @ Andrew Gurylev',
+    keywords: '',
     description: 'thoughtful, attentive, neat, careful, heedful, mindful, solicitous, regardful frontend developer',
     layout: 'layout/layout',
     content: '../about/about'
   },
   '/home': {
     title: 'Homepage',
+    keywords: '',
     description: 'homepage for my browser',
     layout: 'homepage/homepage',
     content: '../grid/grid',
