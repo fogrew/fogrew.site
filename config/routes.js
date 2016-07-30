dfed1c4a92fa0a5a412f5397487af63d66f9548d
Andrew Gurylev <andrey.gurylev@team.wrike.com>
2016-07-31 00:44:11 +0300

    💂 replace format of styles from pcss to scss; fix styllint and eslint configs;

--- config/routes.js
+++ config/routes.js
@@ -4,9 +4,9 @@ const home = require('./home');
 module.exports = {
   '/': {
     title: 'Fogrew @ Andrey Gurylev',
-    keywords: '',
-    image: '',
-    description: 'thoughtful, attentive, neat, careful, heedful, mindful, solicitous, regardful frontend developer',
+    keywords: '', // max 70 characters
+    image: '', // max 1mb
+    description: 'thoughtful, attentive, neat, careful, heedful, mindful, solicitous, regardful frontend developer', // max 200 characters
     layout: 'layout/layout',
     content: '../about/about'
   },
