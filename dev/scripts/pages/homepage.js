73a8c602af2ccd38c95cfe5ac7fbd309cdee557b
Andrey Gurylev <andrey.gurylev@team.wrike.com>
2019-02-25 02:30:02 +0300

    remove all analytics

--- dev/scripts/pages/homepage.js
+++ dev/scripts/pages/homepage.js
@@ -1,5 +1,3 @@
-Raven.config('https://9a43f64e01e54748996958de73cca7ff@sentry.io/117995').install();
-
 let Searcher = require('./homepage/search');
 
 var s = new Searcher();
--- dev/scripts/pages/index.js
+++
@@ -1,1 +0,0 @@
-Raven.config('https://9a43f64e01e54748996958de73cca7ff@sentry.io/117995').install();
